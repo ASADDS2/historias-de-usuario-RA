94ef6845bf00aed22ca293903a20c4cf09e241db
Sebastian ArnacheCantillo <sarnachecantillo@gmail.com>
2025-12-02 18:33:38 -0500

    feat: user history number 3

--- src/main/java/com/riwi/eventsvenues/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.riwi.eventsvenues.exception;
-
-public class NotFoundException extends BusinessException {
-
-    public NotFoundException(String message) {
-        super(message, ErrorCode.NOT_FOUND);
-    }
-
-}
