5097ef8c2727bf00e42ae90a943a88ac28ab1c87
Sebastian ArnacheCantillo <sarnachecantillo@gmail.com>
2025-12-02 18:17:37 -0500

    feat: Historia de usuario semana 2

---
+++ src/main/java/com/riwi/eventsvenues/EventsVenuesApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.eventsvenues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsVenuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsVenuesApplication.class, args);
+    }
+
+}
